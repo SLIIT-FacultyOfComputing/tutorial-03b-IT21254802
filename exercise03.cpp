ce907978fdd4da20c871579f625e812983e7120c
vishwa dlshara <99168165+IT21254802@users.noreply.github.com>
2022-03-05 17:33:41 +0000

    complete_ex3_tu3

--- exercise03.cpp
+++ exercise03.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+
+/*#include <iostream>
 
 using namespace std;
 
@@ -39,5 +40,45 @@ int main() {
     
     return 0;
 }
-
+*/
 // Implement the functions here
+#include <iostream>
+using namespace std;
+
+int volume(int height, int width, int length);
+
+struct box
+{
+	int w , h , l;
+}box1,box2;
+
+int main() {
+	
+ int box1Height, box1Width, box1Length;
+ int box2Height, box2Width, box2Length;
+ int totalVolume, totalSurface;
+
+ cout << "Enter Box 1 Height : ";
+ cin >> box1.h;
+ cout << "Enter Box 1 Width : ";
+ cin >> box1.w;
+ cout << "Enter Box 1 Length : ";
+ cin >> box1.l;
+
+ cout << "Enter Box 2 Height : ";
+ cin >> box2.h;
+ cout << "Enter Box 2 Width : ";
+ cin >> box2.w;
+ cout << "Enter Box 2 Length : ";
+ cin >> box2.l;
+
+ totalVolume = volume(box1.h, box1.w, box1.l)
+ + volume(box2.h, box2.w, box2.l);
+
+ cout << "Volume of Box is " << totalVolume << endl;
+
+ return 0;
+}
+int volume(int height, int width, int length){
+	return height * width * length;
+}
